9cb487f64381b056faf840d1026d4e3ada0e78ff
drplague1 <diebarian1203@yahoo.com>
2023-10-04 20:12:33 -0400

    Fixed some bugs caused by reallocation

--- src/CString.c
+++ src/CString.c
@@ -10,13 +10,28 @@ typedef struct String {
 
 String* StringNew(const char* str) {
     String* string = (String*)malloc(sizeof(String));
-    if(str != NULL) {
+    if (string == NULL) {
+        printf("Error: Memory allocation failed for String struct.\n");
+        exit(1);
+    }
+
+    if (str != NULL) {
         string->length = strlen(str);
-        string->str = (char*)malloc(sizeof(char) * string->length);
-        strcpy_s(string->str, sizeof(string->str), str);
+        string->str = (char*)malloc(sizeof(char) * (string->length + 1));
+        if (string->str == NULL) {
+            printf("Error: Memory allocation failed for string data.\n");
+            free(string);
+            exit(1);
+        }
+        strcpy_s(string->str, sizeof(string->str), str); // Changed strcpy_s to strcpy
     } else {
         string->length = 0;
         string->str = (char*)malloc(sizeof(char));
+        if (string->str == NULL) {
+            printf("Error: Memory allocation failed for string data.\n");
+            free(string);
+            exit(1);
+        }
         string->str[0] = '\0';
     }
     return string;
@@ -38,7 +53,7 @@ void StringAppendChar(String* string, char ch) {
         exit(1);
     }
 
-    if(new_length > 0 && new_length > string->length) {
+    if(new_length > 0 || new_length > string->length) {
         string->str[new_length-1] = ch;
         string->str[new_length] = '\0';
         string->length = new_length;
@@ -64,30 +79,38 @@ int StringGetLength(String* string) {
 }
 
 void StringDestroy(String* string) {
+    if(NULL == string) return;
     free(string->str);
     free(string);
 }
 
 void StringClear(String* string) {
     string->str = (char*)realloc(string->str, sizeof(char)); // resize the string to be 1 char
+
     // check if the realloc was successful
     if(string->str == NULL) {
         printf("Error: On Buffer Variable: string->str is NULL, which means it failed to allocate its memory, most likely\n");
         exit(1);
     }
+
     // Reset the string to be empty (1 char long) -> (always '\0')
     string->str[0] = '\0';
     string->length = 0;
 }
 
 void StringSet(String* string, char* str) {
+    free(string->str);
+    
+    string->str = (char*)malloc(sizeof(str));
+    string->length = strlen(str);
+
     strcpy_s(string->str, sizeof(string->str), str);
 }
 
 void StringSetChar(String* string, int index, char ch) {
-    if(index >= string->length) {
-        //"Index is out of bounds exception in StringSetChar"
+    if(index > string->length || index < 0) {
         printf("ERROR: StringSetChar { Index Out of Bounds Exception }\n");
+        exit(1);
     }
     string->str[index] = ch;
 }
